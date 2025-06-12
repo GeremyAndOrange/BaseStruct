8ee6789160be6fb8ad6fe0c376897653ce385ecd
GeremyAndOrange <605200361@qq.com>
2025-06-12 13:30:32 +0800

    new file:   include/Tree/AVLTree.hpp 	new file:   include/Tree/B+_Tree.hpp 	new file:   include/Tree/B_Tree.hpp 	new file:   include/Tree/BinarySearchTree.hpp 	new file:   include/Tree/BinaryTree.hpp 	new file:   include/Tree/Heap.hpp 	new file:   include/Tree/HuffmanTree.hpp 	new file:   include/Tree/RedBlackTree.hpp 	new file:   include/Tree/Trie.hpp

---
+++ include/Tree/BinaryTree.hpp
@@ -0,0 +1,17 @@
+#ifndef BINARY_TREE_HPP
+#define BINARY_TREE_HPP
+
+#include <iostream>
+
+namespace Tree {
+    template <typename T>
+    class BinaryTree;
+    template <typename T>
+    class BinaryTreeIterator;
+
+    
+}
+
+
+
+#endif
